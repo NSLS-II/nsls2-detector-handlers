cc07ddcdbd0ddfa9ebafc2f81aff6e7a296fe513
jwlodek <jwlodek.dev@gmail.com>
2023-06-02 14:35:42 -0400

    Add webcam module

---
+++ nsls2_detector_handlers/webcam.py
@@ -0,0 +1,11 @@
+from . import HandlerBase
+import h5py
+
+class VideoStreamHDF5Handler(HandlerBase):
+    specs = {"VIDEO_STREAM_HDF5"}
+    def __init__(self, filename):
+        self._name = filename
+    def __call__(self):
+        with h5py.File(self._name, "r") as f:
+            entry = f["/entry/averaged"]
+            return entry[:]
