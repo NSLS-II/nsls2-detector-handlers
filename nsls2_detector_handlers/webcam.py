268532b9a042cf68dc9cf63587ba87afb00e2cd4
Maksim Rakitin <mrakitin@bnl.gov>
2023-06-02 15:11:14 -0400

    Sync handler for webcam with `tiled-site-config` version

--- nsls2_detector_handlers/webcam.py
+++ nsls2_detector_handlers/webcam.py
@@ -5,7 +5,8 @@ class VideoStreamHDF5Handler(HandlerBase):
     specs = {"VIDEO_STREAM_HDF5"}
     def __init__(self, filename):
         self._name = filename
-    def __call__(self):
+
+    def __call__(self, frame):
         with h5py.File(self._name, "r") as f:
             entry = f["/entry/averaged"]
-            return entry[:]
+            return entry[frame, :]
